05654ec10b6377ed46dec4820b40240318a7d984
Rustamnurg <Rustik6030@mail.ru>
2017-04-04 18:38:54 +0300

    finish

--- Project/Lists.swift
+++ Project/Lists.swift
@@ -9,27 +9,55 @@
 import UIKit
 
 class Lists: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let nib = UINib(nibName: "ListsCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "ListsCell")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
+extension Lists: UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListsCell", for: indexPath) as! ListsCell
+       
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+}
+
+
--- Project/OverviewMap.swift
+++ Project/OverviewMap.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MapKit
 
 class OverviewMap: UIViewController {
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
