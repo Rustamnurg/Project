71e9041fa73266dc5540a1d48c33cedc7028935b
Rustamnurg <Rustik6030@mail.ru>
2017-04-22 15:39:39 +0300

    Changed the interface for adding an object

---
+++ Project/AddObject.swift
@@ -0,0 +1,13 @@
+//
+//  AddObject.swift
+//  Project
+//
+//  Created by Rustam N on 22.04.17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+class AddObject: NSObject {
+
+}
